783c61da308b7fa5ac1ba309645f6c1762a70a46
vladyslavtrubnikov <vladyslav.trubnikov@macc.com.ua>
2024-12-10 14:12:12 +0200

    "home-work 4.1"

---
+++ jsBasics/task1.js
@@ -0,0 +1,23 @@
+let newString = "Amazing Word";
+
+let newNumber = 55;
+
+let newBoolean = false;
+
+let newNull = null;
+
+let newUndefined = undefined;
+
+let newDates = new Date()
+
+console.log("Рядок", newString);
+
+console.log("Число", newNumber);
+
+console.log("Булін", newBoolean);
+
+console.log("NUll", newNull);
+
+console.log("Undefined", newUndefined);
+
+console.log("Поточна дата", newDates);
---
+++ jsBasics/task3.js
@@ -0,0 +1,7 @@
+let newUserAge = 17 ///вік нового юзера
+
+let verificationOfAdulthood = 18 /// вік який потрібно для реєстрації
+
+let registration = newUserAge >= verificationOfAdulthood;  /// порівняння віку нового юзера та допустимого віку для авторизації
+
+console.log("Повнолітня особа", registration)
