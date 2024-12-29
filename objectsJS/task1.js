0e7059af0e79de1d06a8227fd4371941bf6fa426
vladyslavtrubnikov <vladyslav.trubnikov@macc.com.ua>
2024-12-29 15:38:11 +0200

    ДЗ 9.1. Objects in JavaScript

---
+++ objectsJS/task1.js
@@ -0,0 +1,10 @@
+const studentObj = {}
+
+studentObj.firstName = "Jerry";
+studentObj.lastName = "Smith";
+studentObj.age = 20;
+studentObj.courses = ["Mathematics", "History", "Programming"];
+
+
+console.log(studentObj);
+
