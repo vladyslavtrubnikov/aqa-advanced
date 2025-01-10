ceab667adf577f8c23e3d78a97ae7f32660c1c74
vladyslavtrubnikov <vladyslav.trubnikov@macc.com.ua>
2025-01-10 17:59:02 +0200

    ДЗ 12.1. Code quality2

---
+++ lesson8.js
@@ -0,0 +1,38 @@
+/// Arrays
+
+// const arr = [1, 2, 3, 4,  [1, 3,[5]]]
+
+// const arr2 = new Array(1, 2, 3, 4, 5,)
+
+// console.log(arr)
+
+// console.log(arr2)
+
+// for(let i = 0; i < arr2.length; i++){
+//     console.log(arr2[i])
+// }
+// console.log('-----------------------')
+
+// for(const num of arr2){
+//     console.log(num)
+// }
+
+//  arr.map((num) => num *2)
+
+// let a = 10;
+// let b = a;
+
+// a =  JSON.parse(JSON.stringify(arr))
+// arr[3] = 'test'
+// arr[4][0] = 'test';
+
+// console.log(a)
+// console.log(arr)
+
+// console.log(b)
+// console.log(arr2)
+
+const sentence = 'Це речення має кілька слів';
+console.log(sentence[0]);
+const words = sentence.split('');
+console.log(words[3]);
